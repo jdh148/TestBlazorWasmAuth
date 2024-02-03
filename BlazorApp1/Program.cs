f36638444581801cd3ab5cfded71247e16a166a0
Jason Hunt <jason@huntconsulting.ca>
2024-02-03 13:31:46 -0500

    Enable authentication login on Blazor.

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -9,6 +9,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiServer = builder.Configuration["ApiServer"] ?? "http://0.0.0.0";
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add(apiServer);
+});
+
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiServer) });
 
 builder.Services.AddHttpClient("WebAPI",
